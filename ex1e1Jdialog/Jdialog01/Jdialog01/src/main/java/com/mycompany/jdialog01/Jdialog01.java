7c17966a8dd3640501249255e374f61db8da351f
Igor Xisto <136636456+IgorXF@users.noreply.github.com>
2024-07-05 22:21:08 -0300

    Add files via upload

---
+++ ex1e1Jdialog/Jdialog01/Jdialog01/src/main/java/com/mycompany/jdialog01/Jdialog01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jdialog01;
+
+import gui.JFrameDepartamento;
+
+
+public class Jdialog01 {
+
+    public static void main(String args[]) {
+        JFrameDepartamento tela = new JFrameDepartamento();
+        tela.setVisible(true);
+    }
+}
