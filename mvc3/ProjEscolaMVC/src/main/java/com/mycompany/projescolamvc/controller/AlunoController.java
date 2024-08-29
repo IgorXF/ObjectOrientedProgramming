2adbbecf20e91e8124cedc447233649e8cb65fa2
Igor Xisto <136636456+IgorXF@users.noreply.github.com>
2024-08-29 13:05:18 -0300

    final

---
+++ mvc3/ProjEscolaMVC/src/main/java/com/mycompany/projescolamvc/controller/AlunoController.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projescolamvc.controller;
+
+import com.mycompany.projescolamvc.model.dao.IDao;
+import com.mycompany.projescolamvc.model.entities.Aluno;
+import com.mycompany.projescolamvc.model.valid.ValidacaoAluno;
+import java.util.List;
+
+/**
+ *
+ * @author igorxf
+ */
+public class AlunoController {
+    private IDao repositorio;
+    
+    public AlunoController(IDao repositorio){
+        this.repositorio = repositorio;
+    }
+    
+    public void adicionarAluno(String cpf, String nome, String idade, String matricula, String anoIngresso) {
+        ValidacaoAluno valid = new ValidacaoAluno();
+        Aluno novoAluno = valid.validacao(cpf, nome, idade, matricula, anoIngresso);
+        
+        repositorio.save(novoAluno);
+    }
+
+    public void removerAluno(String cpf) {
+        repositorio.delete(cpf);
+    }
+
+    public Aluno buscarAluno(String cpf) {
+        return (Aluno) repositorio.find(cpf);
+    }
+
+    public List<Aluno> listarAlunos() {
+        return repositorio.findAll();
+    }
+
+    public void atualizarAluno(String cpfAntigo, String nome, String idade, String matricula, String anoIngresso) {
+        removerAluno(cpfAntigo);
+        adicionarAluno(cpfAntigo, nome, idade, matricula, anoIngresso);
+    }
+}
