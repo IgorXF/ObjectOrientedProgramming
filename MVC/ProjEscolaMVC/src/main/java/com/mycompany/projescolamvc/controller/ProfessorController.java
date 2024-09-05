63a708d1b30f50e8613d0f0f520e4f55ea14b798
Igor Xisto <136636456+IgorXF@users.noreply.github.com>
2024-09-05 20:48:06 -0300

    pronto

---
+++ MVC/ProjEscolaMVC/src/main/java/com/mycompany/projescolamvc/controller/ProfessorController.java
@@ -0,0 +1,48 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projescolamvc.controller;
+
+import com.mycompany.projescolamvc.model.dao.IDao;
+import com.mycompany.projescolamvc.model.entities.Professor;
+import com.mycompany.projescolamvc.model.valid.ValidacaoProfessor;
+import java.util.List;
+
+/**
+ *
+ * @author igorxf
+ */
+public class ProfessorController {
+    private IDao repositorio;
+
+    public ProfessorController(IDao repositorio) {
+        this.repositorio = repositorio;
+    }
+    
+    public void adicionarProfessor(String cpf, String nome, String idade, String sexo) {
+        ValidacaoProfessor valid = new ValidacaoProfessor();
+        Professor novoProfessor = valid.validacao(cpf, nome, idade, sexo);
+        
+        repositorio.save(novoProfessor);
+    }
+
+    public void removerProfessor(String cpf) {
+        repositorio.delete(cpf);
+    }
+
+    public Professor buscarProfessor(String cpf) {
+        return (Professor) repositorio.find(cpf);
+    }
+
+    public List<Professor> listarProfessores() {
+        return repositorio.findAll();
+    }
+
+    public void atualizarProfessor(String cpfAntigo, String cpf, String nome, String idade, String sexo) {
+        ValidacaoProfessor valid = new ValidacaoProfessor();
+        Professor novoP = valid.validacao(cpf, nome, idade, sexo);
+        
+        repositorio.update(cpfAntigo, novoP);
+    }
+}
---
+++ MVC/ProjEscolaMVC/src/main/java/com/mycompany/projescolamvc/model/valid/ValidacaoDisciplina.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projescolamvc.model.valid;
+
+
+import com.mycompany.projescolamvc.model.entities.Disciplina;
+import com.mycompany.projescolamvc.model.exceptions.DisciplinaException;
+
+/**
+ *
+ * @author igorxf
+ */
+public class ValidacaoDisciplina {
+    public Disciplina validacao(String cod,String nome, String semestre, String horario) {
+    Disciplina d = new Disciplina();
+    
+    if(cod.isEmpty())
+        throw new DisciplinaException("Error - Campo vazio: 'Cod'.");
+    d.setCod(cod);
+    
+    if (nome.isEmpty())
+        throw new DisciplinaException("Error - Campo vazio: 'Nome'.");
+    d.setNome(nome);
+    
+    if (semestre.isEmpty())
+        throw new DisciplinaException("Error - Campo vazio: 'Semestre'.");
+    d.setSemestre(semestre);
+    
+    if (horario.isEmpty())
+        throw new DisciplinaException("Error - Campo vazio: 'Horario'.");
+    d.setHorario(horario);
+    
+    
+    return d;
+}
+
+}
